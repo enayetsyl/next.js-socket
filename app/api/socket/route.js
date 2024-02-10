4511b552dd86acd900dee09726e3a6446d29aa9c
Enayet <enayetflweb@gamil.com>
2024-02-10 09:35:16 +0600

    first commit

---
+++ app/api/socket/route.js
@@ -0,0 +1,35 @@
+// pages/api/socket.js
+
+import { Server } from "socket.io";
+
+const socketFunction =  async (req, res) => {
+  if (req.method === "POST") {
+    const io = new Server();
+
+    io.on("connection", (socket) => {
+      console.log(`User Connected: ${socket.id}`);
+
+      socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      });
+
+      socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+      });
+    });
+
+    io.listen(3001); // Adjust the port as needed
+
+    return res.status(200).json({ message: "Socket.io server started" });
+  } else {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+};
+
+
+export default socketFunction
